d70cc9522c3a3ab808c8a00d8a0ca48e5e51591a
Talyn Meyer <talynmeyer77@gmail.com>
2019-11-01 17:47:02 -0600

    Adding setup.py and a test "hello world" function.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import  setup
+
+setup(
+    name= 'GitUtils',
+    version= '0.0.0',
+    packages=[''],
+    url='https://github.com/tjmeyer2/Util-repos.git',
+    license='none',
+    author='Talyn Meyer',
+    author_email='talynmeyer77@gmail.com',
+    description='Some utilities for python'
+)
